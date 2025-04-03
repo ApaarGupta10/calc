d6d3a5550bd6d6a4078678149150c1b89e1161c8
Apaar <apaar2k@gmail.com>
2025-04-03 15:09:44 +0530

    Java Program committed

---
+++ add.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class add {
+  public static void main(String[] args) {
+    scanner sc = new Scanner(System.in);
+    int x=sc.nextInt();
+    int y=sc.nextInt();
+    int sum = x + y;
+    System.out.println(sum);
+  }
+i}
